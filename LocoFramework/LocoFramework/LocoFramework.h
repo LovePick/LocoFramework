143e21a562536a1880f758c5ba37d7cc0671aa0c
supapon <supapon@t2pco.com>
2019-09-19 11:46:26 +0700

    Finished our Loco framerwork

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by T2P mac mini on 19/9/2562 BE.
+//  Copyright © 2562 T2P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,23 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by T2P mac mini on 19/9/2562 BE.
+//  Copyright © 2562 T2P. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    
+    public static func dosomething()->String{
+        
+    return "Did some Stuff, here you go homeboi!"
+    }
+}
